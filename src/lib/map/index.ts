0b5d4c44898f88f59b003e8aedf6e4435bcd6da5
Vanshaj Garg <official.vanshaj.garg@gmail.com>
2026-07-09 00:12:06 +0530

    chore: add map provider config boundary

--- src/lib/map/index.ts
+++ src/lib/map/index.ts
@@ -1,4 +1,6 @@
 export { buildDemandMapViewModel } from "./buildDemandMapViewModel";
+export { DEFAULT_MAP_PROVIDER, getConfiguredMapProvider, isSupportedMapProvider } from "./config";
+export type { MapProviderType } from "./config";
 export type {
   DemandMapCategoryCount,
   DemandMapCategoryFilter,
